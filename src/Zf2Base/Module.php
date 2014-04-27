235fb857fe3c4fd7d11004ce18355df3fa0dda54
Jhon Mike <jhonmike@hotmail.com.br>
2014-04-27 15:31:48 -0300

    Correções

---
+++ src/Zf2Base/Module.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zf2Base;
+
+use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\ModuleManager;
+
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mail\Transport\SmtpOptions;
+
+class Module
+{
+	public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Zf2Base\Mail\Transport' => function($sm) {
+                    $config = $sm->get('Config');
+                    
+                    $transport = new SmtpTransport;
+                    $options = new SmtpOptions($config['mail']);
+                    $transport->setOptions($options);
+                    
+                    return $transport;
+                }
+            )
+        );
+    }
+}
